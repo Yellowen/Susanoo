a42ec021cc788b0c83f9306ee86e39fe00bce3b7
Sameer Rahmani <lxsameer@gnu.org>
2014-01-24 00:24:23 +0330

    some libs added to framework generators

--- lib/Susanoo/generators/frameworks.rb
+++ lib/Susanoo/generators/frameworks.rb
@@ -4,23 +4,37 @@ module Susanoo
       include Thor::Actions
 
       @bower_data = {
-        name: "",
-        dependencies: {},
+        :name => "",
+        :dependencies => {
+          angular: "1.2.6",
+          "angular-touch" => "*",
+          "angular-gesture" => "*",
+        },
       }
+
+      @js_files = ["modernizr/modernizr",
+                   "jquery/jquery",
+                   "lodash/dist/lodash",
+                   "angular-touch/angular-touch",
+                   "angular-gesture/ngGesture/gesture.js"]
+      @css_files = []
 
       def ask_for_framework
 
         @bower_data[:name] = Susanoo::Project.folder_name
-        @bower_data[:dependencies][:angular] = "1.2.6"
 
         if yes? "Do you need Zurb Foundation? (y/n)"
           @bower_data[:dependencies][:foundation] = "*"
+          @css_files << "foundation/scss/foundation"
+          @js_files << "foundation/js/foundation"
           return
         end
         if yes? "What aboud ionic framewor? (y/n)"
           @bower_data[:dependencies][:ionic] = "*"
-        end
+          @js_files << "ionic/js/ionic"
+          @css_files << "ionic/scss/ionic"
 
+        end
 
         inside Susanoo::Project.folder_name do
           inside 'www' do
