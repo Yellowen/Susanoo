0c4bfab4aa6ed4182cafbc74c311f9015d8bed55
Sameer Rahmani <lxsameer@gnu.org>
2014-06-25 00:02:38 +0430

    Application controller module done

--- lib/susanoo/application.rb
+++ lib/susanoo/application.rb
@@ -11,14 +11,22 @@ class Susanoo::Application
 
   class Assets < Susanoo::Controller
     def call(env)
-      [200, {'Content-Type' => 'text/html'}, environment]
+      # [200, {'Content-Type' => 'text/html'}, [environment]]
+      run environment
     end
   end
 
   class Views < Susanoo::Controller
     def call(env)
-      #template = Tilt.new(File.join(project_root, 'www/index.html.erb'))
-      [200, {'Content-Type' => 'text/html'}, ["asdasd"]]
+      path =  env['PATH_INFO']
+      if File.exist?(File.join(project_root, "www/views#{path}.erb"))
+        template = Tilt.new(File.join(project_root, "www/views#{path}.erb"))
+      elsif File.exist?(File.join(project_root, "www/views#{path}"))
+        template = Tilt.new(File.join(project_root, "www/views#{path}"))
+      else
+        fail "There is no '#{path}' in 'www/views' directory."
+      end
+        [200, {'Content-Type' => 'text/html'}, [template.render(self)]]
     end
   end
 
